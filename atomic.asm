c4b20259f31e7c14900e3e96e4f0933d112adea4
dougives <doug.ives@gmail.com>
2016-08-15 08:32:56 -0500

    initial

---
+++ atomic.asm
@@ -0,0 +1,106 @@
+public atomic_lock32
+public atomic_unlock32
+public atomic_lock64
+public atomic_unlock64
+public atomic_set64
+public atomic_set32
+public atomic_increment
+
+; assumes threads aren't using the same rsp...
+
+.code
+
+atomic_lock32 proc
+
+	xor eax, eax
+	lock cmpxchg dword ptr [rcx], esp
+	jne atomic_lock_blocked
+	dec eax
+	ret
+
+atomic_lock_blocked:
+	ret
+
+atomic_lock32 endp
+
+;------------------------------------------------------------------------------
+
+atomic_unlock32 proc
+
+	mov eax, dword ptr [rcx]
+	lock xor dword ptr [rcx], eax
+	ret
+
+atomic_unlock32 endp
+
+;------------------------------------------------------------------------------
+
+atomic_lock64 proc
+
+	xor eax, eax
+	lock cmpxchg qword ptr [rcx], rsp
+	jne atomic_lock_blocked
+	dec eax
+	ret
+
+atomic_lock_blocked:
+	ret
+
+atomic_lock64 endp
+
+;------------------------------------------------------------------------------
+
+atomic_unlock64 proc
+
+	mov rax, qword ptr [rcx]
+	lock xor qword ptr [rcx], rax
+	ret
+
+atomic_unlock64 endp
+
+;------------------------------------------------------------------------------
+
+atomic_set64 proc
+
+	mov rax, qword ptr [rcx]
+	lock cmpxchg qword ptr [rcx], rdx
+	jne atomic_set64_blocked
+	xor eax, eax
+	dec eax
+	ret
+
+atomic_set64_blocked:
+	xor eax, eax
+	ret
+
+atomic_set64 endp
+
+;------------------------------------------------------------------------------
+
+atomic_set32 proc
+
+	mov eax, dword ptr [rcx]
+	lock cmpxchg dword ptr [rcx], edx
+	jne atomic_set32_blocked
+	xor eax, eax
+	dec eax
+	ret
+
+atomic_set32_blocked:
+	xor eax, eax
+	ret
+
+atomic_set32 endp
+
+;------------------------------------------------------------------------------
+
+atomic_increment proc
+
+	lock inc qword ptr [rcx]
+	ret
+
+atomic_increment endp
+
+;------------------------------------------------------------------------------
+
+end
---
+++ atomic.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+
+#define ATOMIC_UNLOCKED 0
+#define ATOMIC_LOCKED  ~0
+
+extern bool atomic_lock32(volatile uint32_t* lock);
+extern void atomic_unlock32(volatile uint32_t* lock);
+extern bool atomic_lock64(volatile uint64_t* lock);
+extern void atomic_unlock64(volatile uint64_t* lock);
+extern bool atomic_set32(volatile uint32_t* addr, uint32_t value);
+extern bool atomic_set64(volatile uint64_t* addr, uint64_t value);
+extern void atomic_increment(volatile uint64_t* addr);
