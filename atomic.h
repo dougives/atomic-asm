dad96081bf546d613add95831f62033a14423928
dougives <doug.ives@gmail.com>
2016-09-27 16:49:53 -0500

    Update atomic.h

--- atomic.h
+++ atomic.h
@@ -16,8 +16,8 @@ extern bool atomic_set64(volatile uint64_t* addr, uint64_t value);
 extern void atomic_increment32(volatile uint32_t* addr);
 extern void atomic_increment64(volatile uint64_t* addr);
 
-extern int atomic_xadd32(volatile uint32_t* addr);
-extern int atomic_xadd64(volatile uint64_t* addr);
+extern uint32_t atomic_xadd32(volatile uint32_t* addr);
+extern uint64_t atomic_xadd64(volatile uint64_t* addr);
 
-extern int atomic_xinc32(volatile uint32_t* addr);
-extern int atomic_xinc64(volatile uint64_t* addr);
+extern uint32_t atomic_xinc32(volatile uint32_t* addr);
+extern uint64_t atomic_xinc64(volatile uint64_t* addr);
