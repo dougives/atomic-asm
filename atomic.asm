f14a83e09fb41e8c895519e1df0dfb63f7821fcd
dougives <doug.ives@gmail.com>
2016-09-27 16:46:54 -0500

    Update atomic.asm

--- atomic.asm
+++ atomic.asm
@@ -142,7 +142,7 @@ atomic_xadd32 endp
 atomic_xinc64 proc
 
 	mov eax, 1
-	lock xadd dword ptr [rcx], eax
+	lock xadd qword ptr [rcx], rax
 	inc eax
 	ret
 
@@ -153,7 +153,7 @@ atomic_xinc64 endp
 atomic_xinc32 proc
 
 	mov eax, 1
-	lock xadd qword ptr [rcx], rax
+	lock xadd dword ptr [rcx], eax
 	inc eax
 	ret
 
