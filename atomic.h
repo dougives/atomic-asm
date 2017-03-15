0a9faddb7415414e4df56fb6e2b87a65c044f7b9
dougives <doug.ives@gmail.com>
2017-03-14 21:49:25 -0500

    also i fixed the xadd proto gonna need rdx lol

--- atomic.h
+++ atomic.h
@@ -16,11 +16,11 @@ extern bool atomic_set64(volatile uint64_t* addr, uint64_t value);
 extern void atomic_increment32(volatile uint32_t* addr);
 extern void atomic_increment64(volatile uint64_t* addr);
 
-extern uint32_t atomic_xadd32(volatile uint32_t* addr);
-extern uint64_t atomic_xadd64(volatile uint64_t* addr);
+extern void atomic_decrement32(volatile uint32_t* addr);
+extern void atomic_decrement64(volatile uint64_t* addr);
+
+extern uint64_t atomic_xadd64(volatile uint64_t* addr, uint64_t value);
+extern uint32_t atomic_xadd32(volatile uint32_t* addr, uint32_t value);
 
 extern uint32_t atomic_xinc32(volatile uint32_t* addr);
 extern uint64_t atomic_xinc64(volatile uint64_t* addr);
-
-extern uint32_t atomic_xdec32(volatile uint32_t* addr);
-extern uint64_t atomic_xdec64(volatile uint64_t* addr);
